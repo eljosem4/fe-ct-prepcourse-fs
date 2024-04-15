c1a8aa74984f92ea86e5ff83fbe126349c9f50a5
eljosem4 <jst_xd@outlook.es>
2024-04-15 17:09:54 -0300

    gg

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,33 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
-}
+ 
+  if (a === 0 || b === 0) {
+    return 0;  
+  }
+
+  if (a === b) {
+    return a;  
+  }
+
+  
+  if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+    return 0;  
+  }
+
+  let resultado = 1;
+
+  if (a <= b) {
+    for (let i = a; i <= b; i++) {
+      resultado *= i;
+    }
+  } else {
+    for (let i = a; i >= b; i--) {
+      resultado *= i;
+    }
+  }
+
+  return resultado;
+}  
 
 module.exports = productoEntreNúmeros;
